f243d4f3336fb8048940be57413d6d0c458274bb
Iván Ruiz Rube <ivan.ruiz@uca.es>
2025-07-05 10:12:15 +0200

    Delete iawebapp directory

--- iawebapp/src/main/java/es/uca/cursoia/aiservices/person/PersonExtractor.java
+++
@@ -1,8 +0,0 @@
-package es.uca.cursoia.aiservices.person;
-
-import dev.langchain4j.service.UserMessage;
-
-interface PersonExtractor {
-    @UserMessage("Extract information about a person from {{it}}")
-    Person extractPersonFrom(String text);
-}
--- iawebapp/src/main/java/es/uca/cursoia/utils/ingestion/IngestionRepository.java
+++
@@ -1,6 +0,0 @@
-package es.uca.cursoia.utils.ingestion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngestionRepository extends JpaRepository<Ingestion, Long> {
-}
